9a8a4a206d215c6116a2f1f9f827479425e3cf3b
imcookiedabadee <ohitscookiemfl@gmail.com>
2023-05-21 20:49:19 -0700

    navbar & projs page
    :wrench:

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import "./style.css"
+function toPage(page) {
+    window.location.href = page + ".html";
+}
+
+document.getElementById("home").addEventListener("click", function () { toPage("index") });
+document.getElementById("projects").addEventListener("click", function () { toPage("projects") });
