d606a4fd3be1fc8d888e3d2c6844e3d97453553c
imcookiedabadee <ohitscookiemfl@gmail.com>
2023-09-25 17:03:15 -0400

    resume
    wowee

--- navbar.js
+++ navbar.js
@@ -4,3 +4,4 @@ function toPage(page) {
 
 document.getElementById("home").addEventListener("click", function () { toPage("index") });
 document.getElementById("projects").addEventListener("click", function () { toPage("projects") });
+document.getElementById("resume").addEventListener("click", function () {window.location.href = "documents/resume.pdf"})
