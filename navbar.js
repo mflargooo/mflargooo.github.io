2fd123cfb36b0dfbc885a35633ee302c56ad3a73
imcookiedabadee <ohitscookiemfl@gmail.com>
2023-12-20 09:18:43 -0800

    photo gallery
    :camera:

--- navbar.js
+++ navbar.js
@@ -9,6 +9,7 @@ if (window.location.href.includes("index"))
 
 document.getElementById("home").addEventListener("click", function () { toPage("index") });
 document.getElementById("projects").addEventListener("click", function () { toPage("projects") });
+document.getElementById("photos").addEventListener("click", function () { toPage("photos") });
 document.getElementById("resume").addEventListener("click", function () { window.open("documents/resume.pdf", "_blank") });
 document.getElementById("about").addEventListener("click", function () {
     if (aboutme)
